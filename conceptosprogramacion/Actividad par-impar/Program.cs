1806e6272172026cded68bdd381686e777ea0799
salazar50100 <samuelsalazar1608@gmail.com>
2025-06-05 19:09:56 -0500

    Solucion actividad par impar

--- conceptosprogramacion/Actividad par-impar/Program.cs
+++ conceptosprogramacion/Actividad par-impar/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿do
+{
+    Console.Write("ingrese un numero: ");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("el numero es par ");
+    }
+    else
+    {
+        Console.WriteLine("el numero es impar ");
+    }
+}
+while (true);
+{
+
+}
